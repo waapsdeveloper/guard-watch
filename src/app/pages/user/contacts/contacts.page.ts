8252cbc9aa1d9875448404d62197167967b91581
shoaibuddin12fx <hamzashahid.waaps@gmail.com>
2023-12-26 15:35:52 +0500

    Updates

--- src/app/pages/user/contacts/contacts.page.ts
+++ src/app/pages/user/contacts/contacts.page.ts
@@ -1,6 +1,8 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { BasePage } from '../../base-page/base-page';
 import { AddContactsComponent } from './add-contacts/add-contacts.component';
+import { ContactsAddComponent } from 'src/app/components/contacts-list/contacts-add/contacts-add.component';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-contacts',
@@ -8,15 +10,57 @@ import { AddContactsComponent } from './add-contacts/add-contacts.component';
   styleUrls: ['./contacts.page.scss'],
 })
 export class ContactsPage extends BasePage implements OnInit {
+  list: any[] = [];
 
-  constructor(injector: Injector) {
-    super(injector)
-   }
+  constructor(injector: Injector, private contact: ContactService) {
+    super(injector);
+    this.getAllcontact()
+  }
 
   ngOnInit() {
+   
   }
-  openAddContacts(){
-    this.modals.present(AddContactsComponent);
+
+  getAllcontact(){
+    return new Promise(async (resolve) => {
+
+      const res = await this.contact.getAllcontact() as any[];
+      console.log('res',res);
+      this.list = res
+      resolve(res);
+
+    })
   }
+
+  setItemColor(item: any){
+
+    if(item.selected == true){
+      return 'light'
+    }else{
+      return ''
+    }
+  }
+
+  async openAddContacts(){
+    console.log('addContact');
+    const res = await this.modals.present(ContactsAddComponent);
+    console.log('res',res);
+
+    this.getAllcontact()
+  }
+
+  async importContact(){
+    console.log('importContact');
+  }
+
+  isListItemSelected(){
+    return this.list.filter(x => x.checked == true).length > 0;
+  }
+
+  selectedContacts(){
+    let list = this.list.filter(x => x.checked == true);
+    this.modals.dismiss({list: list});
+  } 
+  
 
 }
