d9351d784fcea94ed2312891e1a0cd6eab4984e3
shoaib uddin <124657993+HamzaShahidWaaps@users.noreply.github.com>
2024-01-11 12:08:29 +0500

    scan result

--- src/app/pages/guard/dashboard/dashboard.page.ts
+++ src/app/pages/guard/dashboard/dashboard.page.ts
@@ -2,6 +2,8 @@ import { Component, Injector, OnInit } from '@angular/core';
 import { BasePage } from '../../base-page/base-page';
 import { ActionSheetController, AlertController } from '@ionic/angular';
 import { ScanResultPage } from '../scan-result/scan-result.page';
+import { Capacitor } from '@capacitor/core';
+import { InviteService } from 'src/app/services/invite.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,9 +15,8 @@ export class DashboardPage extends BasePage implements OnInit {
   user: any;
   showOptions = false;
   item: any;
-  constructor(injector: Injector, public actionSheet: ActionSheetController, private alertController: AlertController) {
-    super(injector)
-
+  constructor(injector: Injector, private invite: InviteService) {
+    super(injector);
   }
 
   ngOnInit() {
@@ -28,45 +29,34 @@ export class DashboardPage extends BasePage implements OnInit {
 
   async scan() {
     this.showOptions = false;
-    console.log('Scan clicked')
+    console.log('Scan clicked');
+    this.initiateBarcodeScan();
     // await this.presentAlert();
-    this.nav.push('/pages/guard/qrcode-reader')
-
-
+    // this.nav.push('/pages/guard/qrcode-reader')
   }
-  async presentAlert() {
-    const alert = await this.alertController.create({
-      header: 'Enter Id',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-        },
-        {
-          text: 'OK',
-          handler: (data) => {
-            this.handleAlertChoice(data);
-          },
-        },
-      ],
-      inputs: [
-        {
-          label: 'Guard',
-          type: 'number',
-          value: '',
-        },
-      ],
-    });
 
-    await alert.present();
-  }
-  handleAlertChoice(choice: any) {
-    console.log(choice);
-    this.nav.push('/pages/guard/scan-result/' + choice);
-  }
   showPerson() {
     this.showOptions = false;
     console.log('Person clicked');
-    this.nav.push('/pages/guard/resident')
+    this.nav.push('/pages/guard/resident');
+  }
+
+  async initiateBarcodeScan() {
+    if (Capacitor.getPlatform() == 'web') {
+      let array = ['8jenw2GAzbTCTrcgQ9mQ'];
+      const randomElement = array[Math.floor(Math.random() * array.length)];
+      let obj = {
+        qrcode: randomElement,
+      };
+
+      const res = (await this.invite.getQrCodeData(obj)) as any;
+      console.log('Res Data', res);
+      
+      // this.nav.push('/pages/guard/scan-result', {
+      //   object: JSON.stringify(res),
+      // });
+
+      return;
+    }
   }
 }
--- src/app/pages/guard/guard-routing.module.ts
+++ src/app/pages/guard/guard-routing.module.ts
@@ -5,43 +5,57 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'dashboard',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardPageModule)
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardPageModule),
   },
   {
     path: 'resident',
-    loadChildren: () => import('./resident/resident.module').then(m => m.ResidentPageModule)
+    loadChildren: () =>
+      import('./resident/resident.module').then((m) => m.ResidentPageModule),
   },
   {
     path: 'call-resident',
-    loadChildren: () => import('./call-resident/call-resident.module').then(m => m.CallResidentPageModule)
+    loadChildren: () =>
+      import('./call-resident/call-resident.module').then(
+        (m) => m.CallResidentPageModule
+      ),
   },
   {
     path: 'verified-by-guard',
-    loadChildren: () => import('./verified-by-guard/verified-by-guard.module').then(m => m.VerifiedByGuardPageModule)
+    loadChildren: () =>
+      import('./verified-by-guard/verified-by-guard.module').then(
+        (m) => m.VerifiedByGuardPageModule
+      ),
   },
   {
     path: 'pre-approved-guest',
-    loadChildren: () => import('./pre-approved-guest/pre-approved-guest.module').then(m => m.PreApprovedGuestPageModule)
+    loadChildren: () =>
+      import('./pre-approved-guest/pre-approved-guest.module').then(
+        (m) => m.PreApprovedGuestPageModule
+      ),
   },
   {
-    path: 'scan-result/:choice',
-    loadChildren: () => import('./scan-result/scan-result.module').then(m => m.ScanResultPageModule)
+    path: 'scan-result',
+    loadChildren: () =>
+      import('./scan-result/scan-result.module').then(
+        (m) => m.ScanResultPageModule
+      ),
   },
   {
     path: 'qrcode-reader',
-    loadChildren: () => import('./qrcode-reader/qrcode-reader.module').then(m => m.QrcodeReaderPageModule)
-  }
-
+    loadChildren: () =>
+      import('./qrcode-reader/qrcode-reader.module').then(
+        (m) => m.QrcodeReaderPageModule
+      ),
+  },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forChild(routes)
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class GuardRoutingModule { }
+export class GuardRoutingModule {}
